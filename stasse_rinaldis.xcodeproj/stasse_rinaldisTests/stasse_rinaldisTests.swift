8ab2fe1e6e79f745f18fcc383318b42a36bb7584
Damien Rinaldis <115623222+unknownuser717@users.noreply.github.com>
2026-05-29 16:58:31 +0200

    fix swift error

---
+++ stasse_rinaldis.xcodeproj/stasse_rinaldisTests/stasse_rinaldisTests.swift
@@ -0,0 +1,17 @@
+//
+//  stasse_rinaldisTests.swift
+//  stasse_rinaldisTests
+//
+//  Created by Damien Rinaldis on 29/05/2026.
+//
+
+import Testing
+@testable import stasse_rinaldis
+
+struct stasse_rinaldisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
